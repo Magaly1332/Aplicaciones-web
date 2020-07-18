8e100f8feec6b03a832fe47f9b16eee20728077c
Magaly1332 <al221910296@gmail.com>
2020-07-18 18:00:09 -0500

    sentencia commit

---
+++ Sistema/var.php
@@ -0,0 +1,24 @@
+<?php
+ $intVar = 1;
+var_dump ($intVar);
+echo "<br>";
+$floatVar = 1.5;
+var_dump ($floatVar );
+echo "<br>";
+$stringVar = 'IRD-32';
+var_dump ($stringVar );
+echo "<br>";
+$BoolVar = true;
+var_dump($BoolVar);
+echo "<br>";
+
+$arrayVar = [
+  0=> 'valor1IRD-32',
+  1=> 12, 
+  2=> 1.3,
+  3 => true
+  ];
+echo "<br> este es un array ->";
+var_dump($arrayVar);
+
+?>
