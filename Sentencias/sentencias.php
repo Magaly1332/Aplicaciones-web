0949a163bade050feb2b1b105f2761da8c4f1b34
Magaly1332 <al221910296@gmail.com>
2020-07-18 21:52:57 -0500

    sentencias if y for

--- Sentencias/sentencias.php
+++ Sentencias/sentencias.php
@@ -1,24 +1,184 @@
 <?php
-function recoge($var, $m = "")
-{
-    if (!isset($_REQUEST[$var])) {
-        $tmp = (is_array($m)) ? [] : "";
-    } elseif (!is_array($_REQUEST[$var])) {
-        $tmp = trim(htmlspecialchars($_REQUEST[$var], ENT_QUOTES, "UTF-8"));
-    } else {
-        $tmp = $_REQUEST[$var];
-        array_walk_recursive($tmp, function (&$valor) {
-            $valor = trim(htmlspecialchars($valor, ENT_QUOTES, "UTF-8"));
-        });
-    }
-    return $tmp;
+
+echo '<p><H1>Primer ejemplo If (True)</H1></p>';
+
+$Tacos = true;
+
+if($Tacos == true) {
+
+echo 'Si hay tacos';
+
+} else {
+
+echo 'No hay tacos';
+
 }
 
-$nombre = recoge("nombre");
-$edad   = recoge("edad");
-
-print "  <p>Su nombre es <strong>$nombre</strong>.</p>\n";
-print "\n";
-print "  <p>Su edad es <strong>$edad</strong> años.</p>\n";
-print "\n";
 ?>
+
+<?php
+
+echo '<p><H1>Segundo ejemplo If (Quien es el mayor)</H1></p>';
+
+$juan = 40;
+$carlos = 30;
+
+if ($juan > $carlos) {
+
+echo "Sí, Juan es mayor que Carlos.";
+
+} 
+
+else {
+
+echo "No, juan es menor que Carlos";
+
+}
+
+?>
+
+<?php
+
+echo '<p><H1>Tercer ejemplo If (false)</H1></p>';
+
+$Llora = false;
+
+if($Llora == false) {
+
+echo 'Esta triste';
+
+} else {
+
+echo 'Esta feliz';
+
+}
+
+?>
+
+<?php
+
+echo '<p><H1>Cuarto ejemplo If (Numeros pares)</H1></p>';
+
+$numero = 0;
+
+while ($numero <= 50)
+  {
+    $numero++;
+    if ($numero % 2 == 0)
+      {
+        echo "</br> $numero  ";
+      }
+
+  }
+
+
+?>
+
+<?php
+
+echo '<p><H1>Quinto ejemplo If (Numeros Impares)</H1></p>';
+
+$numero = 2;
+
+while ($numero <= 51)
+  {
+    $numero++;
+    if ($numero %2  == 1)
+      {
+        echo "</br> $numero  ";
+      }
+
+  }
+
+?>
+
+<?php
+
+echo '<p><H1>Primer ejemplo For</H1></p>';
+
+$MFE = true;
+
+if($MFE == true) {
+
+echo 'Si existe';
+
+} else {
+
+echo 'No Existe';
+
+}
+
+?>
+
+<?php
+
+echo '<p><H1>Segundo ejemplo For</H1></p>';
+
+$MFE = true;
+
+if($MFE == true) {
+
+echo 'Si existe';
+
+} else {
+
+echo 'No Existe';
+
+}
+
+?>
+
+<?php
+
+echo '<p><H1>Tercer ejemplo If</H1></p>';
+
+$MFE = true;
+
+if($MFE == true) {
+
+echo 'Si existe';
+
+} else {
+
+echo 'No Existe';
+
+}
+
+?>
+
+<?php
+
+echo '<p><H1>Cuarto ejemplo If</H1></p>';
+
+$MFE = true;
+
+if($MFE == true) {
+
+echo 'Si existe';
+
+} else {
+
+echo 'No Existe';
+
+}
+
+?>
+
+<?php
+
+echo '<p><H1>Quinto ejemplo If</H1></p>';
+
+$MFE = true;
+
+if($MFE == true) {
+
+echo 'Si existe';
+
+} else {
+
+echo 'No Existe';
+
+}
+
+?>
+
